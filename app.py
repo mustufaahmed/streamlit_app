df09cfe460fb13cedd77867e65dab5e4008f8dc4
mustufa-ahmed <mustafa.ahmed@salsoft.net>
2022-08-20 17:00:17 +0500

    Fix

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def main():
     st.title("Air Passengers Analysis")
-    data = pd.read_csv("AirPassengers.csv")
+    data = pd.read_csv("https://github.com/mustufaahmed/streamlit_app/blob/main/AirPassengers.csv")
     data['Year'] = data['Month'].apply(lambda x: x.split('-')[0])
     year = st.selectbox('select year', data['Year'].unique())
     button  =  st,button('Show Results')
