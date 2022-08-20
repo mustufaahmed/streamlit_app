5074b847681cb057fc8752b5b93792bc0e1ca4f0
Mustufaahmed <31517776+mustufaahmed@users.noreply.github.com>
2022-08-20 17:27:09 +0500

    Update app.py
    Fix

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def main():
     st.title("Air Passengers Analysis")
-    data = pd.read_csv("https://github.com/mustufaahmed/streamlit_app/blob/main/AirPassengers.csv")
+    data = pd.read_csv("AirPassengers.csv")
     data['Year'] = data['Month'].apply(lambda x: x.split('-')[0])
     year = st.selectbox('select year', data['Year'].unique())
     button  =  st,button('Show Results')
