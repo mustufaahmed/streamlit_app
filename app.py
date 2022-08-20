bd3ecf1f19de43c4a28a8a73aaa56eb893511c69
Mustufaahmed <31517776+mustufaahmed@users.noreply.github.com>
2022-08-20 17:28:06 +0500

    Update app.py
    fix

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def main():
     data = pd.read_csv("AirPassengers.csv")
     data['Year'] = data['Month'].apply(lambda x: x.split('-')[0])
     year = st.selectbox('select year', data['Year'].unique())
-    button  =  st,button('Show Results')
+    button  =  st.button('Show Results')
 
     if button:
         subset = sata[ data['Year'] == year ]
