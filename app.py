1263dc4d995af88ba493c4e139453c9955e2e946
mustufa-ahmed <mustafa.ahmed@salsoft.net>
2022-08-20 16:58:22 +0500

    Change

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
 def main():
-    st.title('Hello World')
+    st.title("Air Passengers Analysis")
+    data = pd.read_csv("AirPassengers.csv")
+    data['Year'] = data['Month'].apply(lambda x: x.split('-')[0])
+    year = st.selectbox('select year', data['Year'].unique())
+    button  =  st,button('Show Results')
+
+    if button:
+        subset = sata[ data['Year'] == year ]
+        st.table(subset)
 
 if __name__== '__main__':
     main()
